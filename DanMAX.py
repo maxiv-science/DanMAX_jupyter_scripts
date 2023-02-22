64e43f90b0f504aeb46d0c27a292cb769d20fd8d
fgjorup <frederik.holm_gjorup@maxiv.lu.se>
2023-02-22 14:10:20 +0100

    Added getMotorSteps
    Added the function getMotorSteps. The function is still a draft and is subject to change.

--- DanMAX.py
+++ DanMAX.py
@@ -426,4 +426,27 @@ def integrate_file(fname, config,embed_meta_data=False):
                     sigma_dset[i] = sigma
         progress.value = i+1
 
+def getMotorSteps(fname):
+    """
+    Return motor name(s), nominal positions, and registred (unique) positions for a given scan.
+        Return list of lists [[motor_name_1,nominal,registred], ...]
+    """
     
+    dic = getMetaDic(fname)
+    scan_type = getScanType(fname).lower().split()
+    motors = [s for s in scan_type if s.islower()][1:]
+    motor_steps = []
+    for motor in motors:
+        # get the nominal motor position from the macro title
+        start, stop, steps = [scan_type[i+1:i+4] for i,s in enumerate(scan_type) if motor in s][0]
+        nominal_pos = np.linspace(float(start),float(stop),int(steps)+1)
+        # get the logged motor position
+        motor_entry_id = [key for key in dic.keys() if motor in key][0]
+        motor_pos = dic[motor_entry_id]
+        motor_pos = np.unique(motor_pos)
+        # compare nominal and actual motor positions
+        if not np.all(nominal_pos == motor_pos):
+            print(f'The nominal and registred motor positions for {motor} do not match!')
+        motor_steps.append([motor,nominal_pos,motor_pos])
+    
+    return motor_steps
